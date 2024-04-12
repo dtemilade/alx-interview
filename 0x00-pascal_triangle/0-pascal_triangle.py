06584f6471d2eff5e1cc138b98bb09b93a4070fd
dtemilade <dtemilade@gmail.com>
2024-04-12 01:23:04 +0100

    submitting commits

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+def generate_pascals_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)  # Initialize each row with 1's
+        for j in range(1, i):  # Exclude the first and last elements
+            try:
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]  # Calculate each element based on the row above
+            except IndexError:
+                pass  # If the index is out of bounds, it means the element is 1 (on the edge of the triangle)
+        triangle.append(row)
+    return triangle
+
+def print_pascals_triangle(triangle):
+    for row in triangle:
+        print(" ".join(map(str, row)))
+
+def pascal_triangle(n):
+    triangle = generate_pascals_triangle(n)
+    return triangle
+
+if __name__ == "__main__":
+    pascal_triangle = pascal_triangle(5)
+    print_pascals_triangle(pascal_triangle)
---
+++ README.md
@@ -0,0 +1,2 @@
+
+# Readme file for ALX INTERVIEW project
