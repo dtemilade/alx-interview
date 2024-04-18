96b5334ba6f9e61994797590525950cb78958922
dtemilade <dtemilade@gmail.com>
2024-04-19 00:04:09 +0100

    Updating commits for task accomplished

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+
+This script determines if all the boxes can be opened.
+
+Args:
+    boxes (list): A list of lists representing the lockboxes and their keys.
+
+Returns:
+    bool: True if all boxes can be opened, False otherwise.
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determine if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists representing the lockboxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+
+    if len(boxes[0]) == 0:
+        return False
+
+    keys = {0}
+    open_boxes = {0}
+    total_boxes = len(boxes)
+    keys = keys.union(boxes[0])
+
+    while total_boxes > 0:
+        for idx in keys:
+            if idx in open_boxes:
+                continue
+            keys = keys.union(boxes[idx])
+            open_boxes.add(idx)
+        total_boxes -= 1
+
+    return len(keys) == len(boxes)
