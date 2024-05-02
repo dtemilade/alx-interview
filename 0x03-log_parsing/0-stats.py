dcae4dc2fc756aebcf0c4cc663c4ea8e2d5812ed
dtemilade <dtemilade@gmail.com>
2024-05-02 13:17:48 +0100

    Updating commits for task accomplished

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,62 @@
 #!/usr/bin/python3
-'''script that reads stdin line by line and computes metrics
+'''
+Script that reads stdin line by line and computes metrics.
 '''
 import re
+import sys
 
 
-def reads_inputs(lines):
-    '''read sections of a line of an HTTP-request log.
+def read_input(line):
     '''
-    fp = (
+    Read sections of a line of an HTTP-request log.
+    '''
+    pattern = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
         r'\s*"(?P<request>[^"]*)"\s*',
         r'\s*(?P<status_code>\S+)',
         r'\s*(?P<var_size>\d+)'
     )
-    info = {
-        'status_code': 0,
-        'var_size': 0,
-    }
-    val_log = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    retval = re.fullmatch(val_log, lines)
-    if retval is not None:
-        status_code = retval.group('status_code')
-        var_size = int(retval.group('var_size'))
+    info = {'status_code': 0, 'var_size': 0}
+    line_pattern = '{}\\-{}{}{}{}\\s*'.format(*pattern)
+    match = re.fullmatch(line_pattern, line)
+    if match is not None:
+        status_code = match.group('status_code')
+        var_size = int(match.group('var_size'))
         info['status_code'] = status_code
         info['var_size'] = var_size
     return info
 
 
-def stat_retval(total_var_size, status_codes_stats):
-    '''Output the accumulated statistics.
+def print_statistics(total_var_size, status_codes_stats):
     '''
-    print('File size: {:d}'.format(total_var_size), flush=True)
+    Output the accumulated statistics.
+    '''
+    print('File size:', total_var_size)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
         if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+            print('{}: {}'.format(status_code, num))
 
 
-def update_val(line, total_var_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP-request.
+def update_metrics(line, total_var_size, status_codes_stats):
+    '''
+    Updates the metrics from a given HTTP-request.
     Args:
         line (str): The line of input to retrieve the metrics.
     Returns:
         int: The new total file size.
     '''
-    line_info = reads_inputs(line)
+    line_info = read_input(line)
     status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
+    if status_code in status_codes_stats:
         status_codes_stats[status_code] += 1
     return total_var_size + line_info['var_size']
 
 
 def run():
-    '''Running the log parser.
+    '''
+    Running the log parser.
     '''
     line_num = 0
     total_var_size = 0
@@ -68,18 +71,13 @@ def run():
         '500': 0,
     }
     try:
-        while True:
-            line = input()
-            total_var_size = update_val(
-                line,
-                total_var_size,
-                status_codes_stats,
-            )
+        for line in sys.stdin:
+            total_var_size = update_metrics(line, total_var_size, status_codes_stats)
             line_num += 1
             if line_num % 10 == 0:
-                stat_retval(total_var_size, status_codes_stats)
-    except (KeyboardInterrupt, EOFError):
-        stat_retval(total_var_size, status_codes_stats)
+                print_statistics(total_var_size, status_codes_stats)
+    except KeyboardInterrupt:
+        print_statistics(total_var_size, status_codes_stats)
 
 
 if __name__ == '__main__':
