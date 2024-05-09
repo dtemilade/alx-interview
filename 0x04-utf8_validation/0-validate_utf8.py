de5e72235093427fc647438853e079ad9bac1dfe
dtemilade <dtemilade@gmail.com>
2024-05-09 10:09:32 +0100

    Updating commits for task accomplished

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+''' Method that determines if a given data set
+    represents a valid UTF-8 encoding.
+'''
+
+
+def validUTF8(data):
+    skip = 0
+    data_len = len(data)
+    for i in range(data_len):
+        if skip > 0:
+            skip -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            span = 4
+            if data_len - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11110000 == 0b11100000:
+            span = 3
+            if data_len - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            span = 2
+            if data_len - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        else:
+            return False
+    return True
