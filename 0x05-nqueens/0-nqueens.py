f30981e2883e895a10f1996ca59fbbf19bf05132
dtemilade <dtemilade@gmail.com>
2024-05-16 23:13:49 +0100

    Updating commits for task accomplished

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-'''
-Script for The N queens puzzle.
+''' Script for The N queens puzzle.
 '''
 import sys
 
@@ -18,16 +17,17 @@ pos = None
 
 def get_input():
     '''Accepts and validates inputs from user.
-    Returns the input in int type.
-    Exceptions are defined accordingly.
+    Returns:
+        the input in int type. Exceptions are defined accordingly.
     '''
     global n
+    n = 0
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except ValueError:
+    except Exception:
         print("N must be a number")
         sys.exit(1)
     if n < 4:
@@ -37,14 +37,14 @@ def get_input():
 
 
 def is_attacking(pos1, pos2):
-    '''Method to check if two queens are in an attacking mode.
+    '''Method for two queens are in an attacking mode.
     Args:
-        pos1: The first queen's position.
-        pos2: The second queen's position.
+        pos1 contains the first queen's position.
+        pos2 contains the second queen's position.
     Returns:
-        True if queens are in an attacking position otherwise False.
+        True for queens are in an attacking position otherwise False.
     '''
-    if pos1[0] == pos2[0] or pos1[1] == pos2[1]:
+    if (pos1[0] == pos2[0]) or (pos1[1] == pos2[1]):
         return True
     return abs(pos1[0] - pos2[0]) == abs(pos1[1] - pos2[1])
 
@@ -52,45 +52,56 @@ def is_attacking(pos1, pos2):
 def group_checking(group):
     '''Checks if a group exists in the list of return values.
     Args:
-        group: Possible positions.
+        Group are possible positions.
     Returns:
         True if it exists otherwise False.
     '''
     global retval
-    for solution in retval:
-        if all(any(pos1 == pos2 for pos2 in solution) for pos1 in group):
+    for chars in retval:
+        i = 0
+        for chars_pos in chars:
+            for grp_pos in group:
+                if chars_pos[0] == grp_pos[0] and chars_pos[1] == grp_pos[1]:
+                    i = i + 1
+        if i == n:
             return True
     return False
 
 
 def build_val(row, group):
-    '''Finds possible solutions for the n queens problem.
+    '''Method for posible solution for the n queens
     Args:
-        row (int): The current row on the chessboard.
-        group (list of lists): The group of valid positions.
+        row (int): The current row in the chessboard.
+        group (list of lists of integers): The group of valid positions.
     '''
-    global retval, n
+    global retval
+    global n
     if row == n:
-        if not group_checking(group):
-            retval.append(group.copy())
+        tmp0 = group.copy()
+        if not group_checking(tmp0):
+            retval.append(tmp0)
     else:
         for col in range(n):
-            pos_index = row * n + col
-            if all(not is_attacking(pos[pos_index], placed) for placed in group):
-                group.append(pos[pos_index])
+            a = (row * n) + col
+            matches = zip(list([pos[a]]) * len(group), group)
+            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
+            group.append(pos[a].copy())
+            if not any(used_positions):
                 build_val(row + 1, group)
-                group.pop()
+            group.pop(len(group) - 1)
 
 
 def get_retval():
-    '''Finds the final solution for the given chessboard size.
+    '''Method for final solution for the given chessboard size.
     '''
     global pos, n
     pos = [[i // n, i % n] for i in range(n ** 2)]
-    build_val(0, [])
+    a = 0
+    group = []
+    build_val(a, group)
 
 
 n = get_input()
 get_retval()
-for solution in retval:
-    print(solution)
+for val in retval:
+    print(val)
