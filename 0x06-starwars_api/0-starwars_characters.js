6da4fcabefb78aab82d08c015b63d29466c78d01
dtemilade <dtemilade@gmail.com>
2024-05-24 10:02:11 +0100

    Updating commits for task accomplished

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,18 +3,18 @@ const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
-async function starwarsretval (filmId) {
-  const endpoint = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
-  let res = await (await request(endpoint)).body;
-  res = JSON.parse(res);
-  const retval = res.retval;
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-  for (let i = 0; i < retval.length; i++) {
-    const retval = retval[i];
-    let chars = await (await request(retval)).body;
-    chars = JSON.parse(chars);
-    console.log(chars.name);
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
   }
 }
 
-starwarsretval(filmID);
+starwarsCharacters(filmID);
